32ec8bb5b5e29861d3dc3e4074034cf6e5a95029
Yoanna Popova <yoanna.dp@gmail.com>
2022-08-16 08:23:02 +0300

    Made changes to main div and site header elements.

--- functionality/header-component.js
+++ functionality/header-component.js
@@ -1,5 +1,5 @@
-let divSiteHeader = document.getElementsByClassName('site-header');
+let mainDivElement = document.querySelector('.site-header');
 
-let headerSiteLogo = document.createElement('header');
-headerSiteLogo.setAttribute('class', 'site-logo');
-divSiteHeader.appendChild(headerSiteLogo);
+let headerElement = document.createElement('header');
+headerElement.setAttribute('class', 'site-logo');
+mainDivElement.appendChild(headerElement);
