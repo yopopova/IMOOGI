34754fd3e288b9ebeea3e18a68bbf5375a36bc59
Yoanna Popova <yoanna.dp@gmail.com>
2022-08-16 08:31:36 +0300

    Append link element to header element.

--- functionality/header-component.js
+++ functionality/header-component.js
@@ -3,3 +3,7 @@ let mainDivElement = document.querySelector('.site-header');
 let headerElement = document.createElement('header');
 headerElement.setAttribute('class', 'site-logo');
 mainDivElement.appendChild(headerElement);
+
+let linkLogoElement = document.createElement('a');
+linkLogoElement.setAttribute('href', 'index.html');
+headerElement.appendChild(linkLogoElement);
