910ed9567bfd5e0f87c2b0c34a510fdcca89fef5
Yoanna Popova <yoanna.dp@gmail.com>
2022-09-01 09:38:24 +0300

    Add main div elements to site footer.

--- functionality/footer-component.js
+++ functionality/footer-component.js
@@ -1,1 +1,11 @@
 let footerElement = document.querySelector('.site-footer');
+
+// Main div element
+let mainFooterDivElement = document.createElement('div');
+mainFooterDivElement.setAttribute('class', 'footer-main');
+footerElement.appendChild(mainFooterDivElement);
+
+// Footer menu
+let menuFooterElement = document.createElement('div');
+menuFooterElement.setAttribute('class', 'footer-menu');
+mainFooterDivElement.appendChild(menuFooterElement);
