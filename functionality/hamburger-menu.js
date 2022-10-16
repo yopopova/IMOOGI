661fa6193c9f82959187fcc97b1f09bbb4bf6e40
Yoanna Popova <yoanna.dp@gmail.com>
2022-10-16 10:09:32 +0300

    Remove the file with hamburger menu functionality.

--- functionality/hamburger-menu.js
+++
@@ -1,8 +0,0 @@
-let hamburgerMenuEl = document.querySelector('.hamburger-menu');
-let menuEl = document.querySelector('.menu');
-
-// Show the menu and transform hamburger menu into 'X'
-hamburgerMenuEl.addEventListener('click', () => {
-    hamburgerMenuEl.classList.toggle('active');
-    menuEl.classList.toggle('active');
-})
